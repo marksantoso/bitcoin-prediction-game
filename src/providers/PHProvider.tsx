7481f0dafe6b3577e94bbb145248e9a8743db33f
Mark Santoso <marksantoso@gmail.com>
2025-06-17 15:39:34 +0200

    feat: fix misnomer in post hog page view tracker

--- src/providers/PHProvider.tsx
+++ src/providers/PHProvider.tsx
@@ -7,7 +7,7 @@ import { useEffect } from "react"
 import posthog from 'posthog-js'
 import { PostHogProvider as PHProvider } from 'posthog-js/react'
 
-function SuspendedPostHogPageView() {
+function PostHogPageView() {
   const pathname = usePathname()
   const searchParams = useSearchParams()
 
@@ -25,13 +25,13 @@ export function PostHogProvider({ children }: { children: React.ReactNode }) {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
       api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
       person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
-      defaults: '2025-05-24'
+      defaults: '2025-05-24',
     })
   }, [])
 
   return (
     <PHProvider client={posthog}>
-      <SuspendedPostHogPageView />
+      <PostHogPageView />
       {children}
     </PHProvider>
   )
